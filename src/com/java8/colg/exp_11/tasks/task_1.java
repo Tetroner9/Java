76ef9cac4f799f5d4bef4c324c3360c8764cca14
Tetroner9 <omkadam2125@gmail.com>
2022-10-24 13:53:14 +0530

    ok

---
+++ src/com/java8/colg/exp_11/tasks/task_1.java
@@ -0,0 +1,31 @@
+package com.java8.colg.exp_11.tasks;
+import java.util.*;
+
+class NameLimit extends Exception
+{
+    public NameLimit(String s)
+    {
+        super(s);
+    }
+}
+class CustomException
+{
+
+    void LetterLimit(String s) throws NameLimit
+    {
+        int s_length = s.length();
+        if (s_length>10)
+            System.out.println("Name should be less than 10 letters!");
+        else
+            System.out.println("Welcome");
+    }
+}
+public class task_1 {
+    public static void main(String[] args) throws NameLimit {
+        Scanner sc = new Scanner(System.in);
+        CustomException ce = new CustomException();
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        ce.LetterLimit(name);
+    }
+}
